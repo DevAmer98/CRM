b0287f753960a4f020fc27c2abf2b4f866855ee1
DevAmer98 <devamer98@gmail.com>
2025-06-11 16:34:40 +0300

    tekrar

--- app/api/currentUser/route.js
+++ app/api/currentUser/route.js
@@ -1,4 +1,4 @@
-import { getSession } from "next-auth/react";
+/*import { getSession } from "next-auth/react";
 import {NextResponse } from "next/server";
 
 export const revalidate = 0; 
@@ -14,3 +14,21 @@ export default async function GET(req, res) {
   }
 }
 
+*/
+
+
+import { getServerSession } from "next-auth";
+import { authConfig } from "@/app/authconfig" // adjust this import if needed
+import { NextResponse } from "next/server";
+
+export const revalidate = 0;
+
+export async function GET(req) {
+  const session = await getServerSession(authConfig);
+
+  if (session?.user) {
+    return NextResponse.json({ user: session.user });
+  } else {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+}
