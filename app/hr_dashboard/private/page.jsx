7918b961384144144d498a4c33ed4d4430c0042f
DevAmer98 <devamer98@gmail.com>
2026-02-09 13:50:35 +0300

    hg

--- app/hr_dashboard/private/page.jsx
+++ app/hr_dashboard/private/page.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import styles from '../../ui/hr_dashboard/hr_dashboard.module.css';
 import { Suspense } from 'react';
 import ErrorBoundary from '../../ui/dashboard/errorBoundary/errorBoundary';
-import HR_AdminPage from '@/app/ui/private_dashbaord/hr_admin';
+import AdminPage from '@/app/ui/private_dashbaord/admin';
 
 const PrivateDashboard = () => {
   return (
@@ -11,7 +11,7 @@ const PrivateDashboard = () => {
       <Suspense fallback={<div>Loading...</div>}>
         <div className={styles.wrapper}>
           <div className={styles.main}>
-            <HR_AdminPage />
+            <AdminPage />
           </div>
         </div>
       </Suspense>
